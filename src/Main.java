3fe6d80bd562f5cf1dcc5a0fdab3a0daaa7d2611
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:37:35 +0500

    BMI homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        double weight;
+        double height;
+        try {
+
+            System.out.println("Привет, это калькулятор индекса массы тела, для начала введи свой вес");
+            weight = scanner.nextDouble();
+            System.out.println("Отлично твой вес:" + weight + " " + "теперь введи свой рост в метрах");
+            height = scanner.nextDouble();
+            double bmi = Math.round (weight / (height * height));
+            if (bmi <= 18.5){
+                System.out.println("Ты дрыщ, твой ИМТ:" + bmi);
+            }  else if (bmi <= 25.0) {
+                System.out.println("Ты гигачад, твой ИМТ:" + bmi);
+            } else if (bmi <= 30.0)
+                System.out.println("Ты толстячок, твой ИМТ" + bmi);
+            else if (bmi > 30) System.out.println("Ты жирнич, твой ИМТ:" + bmi);
+        }
+        catch (Exception ex){
+            System.out.println("Нормально же всё объяснил, а ты ввёл что-то не так, перезагружай прогу теперь");
+        }
     }
 }
